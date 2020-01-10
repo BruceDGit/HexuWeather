f9d6954fba525877fb2ee2e9c35ebc3e6904921c
Bruce <wayne_s@126.com>
2020-01-10 19:54:00 +0800

    alter the comments

--- Data/API/crontab_code.py
+++ Data/API/crontab_code.py
@@ -6,19 +6,19 @@
 
 
 """
-# 实况天气	10-20分钟更新一次
+# 实况天气	每天更新一次
 # weather_now_insert
 27 8 * * * /home/ubuntu/anaconda3/bin/python3.6 /home/ubuntu/project/weather_spider/apis/weather_now_insert.py  >>/home/ubuntu/logs/HexuWeather/api/crontab_logs/weather_now_insert.log 2>&1
 
-# 生活指数	每天更新3次，分别在8，11，18点左右更新
+# 生活指数	每天更新一次
 # weather_lifeindex_insert
 27 9 * * * /home/ubuntu/anaconda3/bin/python3.6 /home/ubuntu/project/weather_spider/apis/weather_lifeindex_insert.py >>/home/ubuntu/logs/HexuWeather/api/crontab_logs/weather_lifeindex_insert.log 2>&1
 
-# 今日天气	每小时更新一次
+# 今日天气	每天更新一次
 # weather_today_insert
 27 8 * * * /home/ubuntu/anaconda3/bin/python3.6 /home/ubuntu/project/weather_spider/apis/weather_today_insert.py >>/home/ubuntu/logs/HexuWeather/api/crontab_logs/weather_today_insert.log 2>&1
 
-# 7日天气	每小时更新一次
+# 7日天气	每天更新一次
 # weather_7days_insert
 27 8 * * * /home/ubuntu/anaconda3/bin/python3.6 /home/ubuntu/project/weather_spider/apis/weather_7days_insert.py >>/home/ubuntu/logs/HexuWeather/api/crontab_logs/weather_7days_insert.log 2>&1
 """
